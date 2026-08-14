1830a749e1a19992439ed61962aef5ea330362c5
bepevner <niumma1130@gmail.com>
2026-08-14 11:21:46 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase 콘솔(console.firebase.google.com) > 프로젝트 설정 > 웹 앱 추가에서
 // 발급받은 값을 아래에 그대로 붙여넣으세요.
 const firebaseConfig = {
-  apiKey: "여기에_API_KEY",
-  authDomain: "여기에_PROJECT_ID.firebaseapp.com",
-  projectId: "여기에_PROJECT_ID",
-  storageBucket: "여기에_PROJECT_ID.appspot.com",
-  messagingSenderId: "여기에_SENDER_ID",
-  appId: "여기에_APP_ID",
+  apiKey: AIzaSyBJPO4tP-bLkc5WPJohVsj0-eA5s_9k5M8,
+  authDomain: edde-86754.firebaseapp.com,
+  projectId: edde-86754,
+  storageBucket: edde-86754.firebasestorage.app,
+  messagingSenderId: 500519498865,
+  appId: 1:500519498865:web:c0e16c7e6a47f302f457d0,
 };
 
 firebase.initializeApp(firebaseConfig);
